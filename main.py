6110a51da3d80343369bf399264549eac2a953e4
Davi <davicamiloribeiro@gmail.com>
2022-11-26 00:04:52 -0300

    Importanto bibliotecas

---
+++ main.py
@@ -0,0 +1,12 @@
+# Bibliotecas de uso geral
+import sys
+import os
+import time, random
+
+# Bibliotecas para manipulação e construção da interface gráfica -----
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+import tkinter.font as font
+
+# Espaço de Tuplas
+tse = None
