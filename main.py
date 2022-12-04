2b536dd304e6a27de427754877ac3f509f753f46
Davi <davicamiloribeiro@gmail.com>
2022-12-04 01:19:27 -0300

    Criação dos containers de processos

--- main.py
+++ main.py
@@ -10,3 +10,54 @@ import tkinter.font as font
 
 # Espaço de Tuplas
 tse = None
+
+# Lista que vai registrar o nome dos usuários criados e sua 'flags' de recebimento de novas msgs ('0' quando não detectou nenhuma msg nova e '1' quando detectou)
+lista_registro_nuvens = []
+
+# Criação da Nuvem e das matrizes para armazenar as NUVENS
+def criarNuvem(ts, nomeNuvem):
+    initNuvem = []
+    ts.out(("NUVEM", nomeNuvem, tuple(initNuvem)))
+
+    nuvens = ts.inp(("NUVENS", object))
+    temp = list(nuvens[1])
+    temp.append(nomeNuvem)
+    ts.out(("NUVENS", tuple(temp)))
+
+# Criação do Host e das matrizes para armazenar os HOSTS
+def criarHost(ts, nomeHost):
+    initHost = []
+    ts.out(("HOST", nomeHost, tuple(initHost)))
+
+    hosts = ts.inp(("HOSTS", object))
+    temp = list(hosts[1])
+    temp.append(nomeHost)
+    ts.out(("HOSTS", tuple(temp)))
+
+# Criação do Host e das matrizes para armazenar os HOSTS
+def criarVM(ts, nomeVM):
+    initVM = []
+    ts.out(("VM", nomeVM, tuple(initVM)))
+
+    vms = ts.inp(("VMS", object))
+    temp = list(vms[1])
+    temp.append(nomeVM)
+    ts.out(("VMS", tuple(temp)))
+
+# Cria a tupla do Processo e coloca ela na tupla 'PROCESSOS' para ser armazenada
+def criarProcesso(ts, nomeProcesso):
+    processos = ts.inp(("PROCESSOS", object))
+
+    temp = list(processos[1])
+    temp.append(nomeProcesso)
+    ts.out(("PROCESSOS", tuple(temp)))
+
+'''
+
+'''
+# Listar TODAS as NUVENS do 'ts' ('Tuple Space')
+def listarNuvens(ts):
+    nuvens = ts.rdp(("NUVENS", object))
+    print(nuvens)
+    
+    return list(nuvens[1])
